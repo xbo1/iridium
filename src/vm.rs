d863ce40f1dbdec2db2cfaacc92acfd739096932
bob <wangshyn@gmail.com>
2018-08-24 14:37:28 +0800

    参考：https://blog.subnetzero.io/post/building-language-vm-part-02/

--- src/vm.rs
+++ src/vm.rs
@@ -1,12 +1,43 @@
+use instruction::Opcode;
+
 pub struct VM {
-    registers: [i32;32]
+    registers: [i32;32],
+    pc: usize,
+    program: Vec<u8>
 }
 
 impl VM {
     pub fn new() -> VM {
         VM {
-            registers: [0;32]
+            registers: [0;32],
+            program: vec![],
+            pc: 0,
         }
+    }
+
+    pub fn run(&mut self) {
+        loop {
+            // If our program counter has exceeded the length of the program itself, something has
+            // gone awry
+            if self.pc >= self.program.len() {
+                break;
+            }
+            match self.decode_opcode() {
+                Opcode::HLT => {
+                    println!("HLT encountered");
+                    return;
+                },
+                _ => {
+                println!("Unrecognized opcode found! Terminating!");
+                return;
+                }
+            }
+        }
+    }
+    fn decode_opcode(&mut self) -> Opcode {
+        let opcode = Opcode::from(self.program[self.pc]);
+        self.pc += 1;
+        return opcode;
     }
 }
 
@@ -19,5 +50,23 @@ mod tests {
     fn test_create_vm() {
         let test_vm = VM::new();
         assert_eq!(test_vm.registers[0],0);
+    }
+
+    #[test]
+    fn test_opcode_hlt() {
+      let mut test_vm = VM::new();
+      let test_bytes = vec![0,0,0,0];
+      test_vm.program = test_bytes;
+      test_vm.run();
+      assert_eq!(test_vm.pc, 1);
+    }
+
+    #[test]
+    fn test_opcode_igl() {
+      let mut test_vm = VM::new();
+      let test_bytes = vec![200,0,0,0];
+      test_vm.program = test_bytes;
+      test_vm.run();
+      assert_eq!(test_vm.pc, 1);
     }
 }
